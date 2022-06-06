b9a142f6935bb157f9a2c22c91b3e25537d28369
Bikash Sahani <bsahani26@gmail.com>
2022-06-06 13:08:01 +0530

    Implemented sliver list

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,69 @@ void main() {
   runApp(SliverListApp());
 }
 
+const foodDetails =
+    "Healthy eating doesn’t have to be overly complicated. If you feel overwhelmed by all the conflicting nutrition and diet advice out there, you’re not alone. It seems that for every expert who tells you a certain food is good for you, you’ll find another saying exactly the opposite. The truth is that while some specific foods or nutrients have been shown to have a beneficial effect on mood, it’s your overall dietary pattern that is most important. The cornerstone of a healthy diet should be to replace processed food with real food whenever possible. Eating food that is as close as possible to the way nature made it can make a huge difference to the way you think, look, and feel.By using these simple tips, you can cut through the confusion and learn how to create—and stick to—a tasty, varied, and nutritious diet that is as good for your mind as it is for your body. Healthy eating doesn’t have to be overly complicated. If you feel overwhelmed by all the conflicting nutrition and diet advice out there, you’re not alone. It seems that for every expert who tells you a certain food is good for you, you’ll find another saying exactly the opposite. The truth is that while some specific foods or nutrients have been shown to have a beneficial effect on mood, it’s your overall dietary pattern that is most important. The cornerstone of a healthy diet should be to replace processed food with real food whenever possible. Eating food that is as close as possible to the way nature made it can make a huge difference to the way you think, look, and feel. By using these simple tips, you can cut through the confusion and learn how to create—and stick to—a tasty, varied, and nutritious diet that is as good for your mind as it is for your body. Healthy eating doesn’t have to be overly complicated. If you feel overwhelmed by all the conflicting nutrition and diet advice out there, you’re not alone. It seems that for every expert who tells you a certain food is good for you, you’ll find another saying exactly the opposite. The truth is that while some specific foods or nutrients have been shown to have a beneficial effect on mood, it’s your overall dietary pattern that is most important. The cornerstone of a healthy diet should be to replace processed food with real food whenever possible. Eating food that is as close as possible to the way nature made it can make a huge difference to the way you think, look, and feel. By using these simple tips, you can cut through the confusion and learn how to create—and stick to—a tasty, varied, and nutritious diet that is as good for your mind as it is for your body. Healthy eating doesn’t have to be overly complicated. If you feel overwhelmed by all the conflicting nutrition and diet advice out there, you’re not alone. It seems that for every expert who tells you a certain food is good for you, you’ll find another saying exactly the opposite. The truth is that while some specific foods or nutrients have been shown to have a beneficial effect on mood, it’s your overall dietary pattern that is most important. The cornerstone of a healthy diet should be to replace processed food with real food whenever possible. Eating food that is as close as possible to the way nature made it can make a huge difference to the way you think, look, and feel. By using these simple tips, you can cut through the confusion and learn how to create—and stick to—a tasty, varied, and nutritious diet that is as good for your mind as it is for your body.";
+
 class SliverListApp extends StatelessWidget {
   const SliverListApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: CustomScrollView(
-        slivers: [SliverAppBar()],
+        slivers: [
+          SliverAppBar(
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                  Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  )
+                ]),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(30),
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
+                    )),
+                width: double.maxFinite,
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: const Center(
+                    child: Text(
+                  "Food Health Details",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                )),
+              ),
+            ),
+            pinned: true,
+            backgroundColor: Colors.lightBlue[50],
+            expandedHeight: 220,
+            flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset(
+              "assets/images/food.jpg",
+              width: double.maxFinite,
+              fit: BoxFit.cover,
+            )),
+          ),
+          SliverToBoxAdapter(
+              child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: const Text(
+                    foodDetails,
+                    textAlign: TextAlign.justify,
+                  )))
+        ],
       )),
     );
   }
